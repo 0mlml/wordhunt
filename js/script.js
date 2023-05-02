f590c50fa38b5865cbb33deeaca3a259945eb475
m <63015852+0mlml@users.noreply.github.com>
2023-05-02 01:05:16 -0400

    Input validation

--- js/script.js
+++ js/script.js
@@ -7,8 +7,17 @@ const nodesElement = document.getElementById('nodes');
 
 const solver = new Solver(gridElement, resultsElement, nodesElement);
 solver.updateInput('catdwofoirdgnoas');
+solver.startSolving();
 
 solveElement.addEventListener('click', () => {
+  if (!solver.stopped) {
+    solver.stopSolving();
+    return;
+  }
+  if (inputElement.value.length !== 16) {
+    alert('Input must be 16 characters long');
+    return;
+  }
   solver.updateInput(inputElement.value);
   solver.startSolving();
 });
