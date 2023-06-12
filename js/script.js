4857fd597ae571684e409a9459174b654fa56b48
ml <63015852+0mlml@users.noreply.github.com>
2023-06-11 20:40:19 -0400

    New layouts + URL params
    -Grid is now set in URL param
    --Allows reloading and maintaining state
    --Allows creating URL to specific solution
    -Add support for premium grid shapes from gamepigeon
    --Inferred from the amount of letters
    -Different formatter, a bunch of small changes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,55 @@
-const inputElement = document.getElementById('grid-input');
-const solveElement = document.getElementById('solve-button');
+const inputElement = document.getElementById("grid-input");
+const solveElement = document.getElementById("solve-button");
 
-const gridElement = document.getElementById('grid');
-const gridOverlayElement = document.getElementById('grid-overlay');
-const resultsElement = document.getElementById('results-list');
+const gridElement = document.getElementById("grid");
+const gridOverlayElement = document.getElementById("grid-overlay");
+const resultsElement = document.getElementById("results-list");
 
 const solver = new Solver(gridElement, gridOverlayElement, resultsElement);
-solver.updateInput('catdwofoirdgnoas');
+
+const params = new URLSearchParams(window.location.search);
+let gridString = params.has("grid") ? params.get("grid").toLowerCase() : null;
+
+if (gridString && gridString.replace(/\W/g, "") !== gridString) {
+	console.error("Invalid URL 'grid' parameter. (Non-alphabetical characters present)");
+} else if (gridString && !layouts.find(e => e.tiles === gridString.length)) {
+	console.error("Invalid URL 'grid' parameter. (Length not any of existing layouts)");
+	gridString = null;
+}
+
+if (!gridString) {
+	gridString = "catdwofoirdgnoas";
+}
+
+solver.updateInput(gridString);
 solver.startSolving();
 
-solveElement.addEventListener('click', () => {
+
+const updateURLParams = () => {
+	const url = new URL(window.location.href);
+	url.searchParams.set("grid", solver.input);
+	window.history.replaceState(null, null, url);
+}
+
+solveElement.addEventListener("click", () => {
 	if (!solver.stopped) {
 		solver.stopSolving();
 	}
-	if (inputElement.value.length !== 16) {
-		alert('Input must be 16 characters long');
+	if (!layouts.find(e => e.tiles === gridString.length)) {
+		alert(`Input length must be any of: ${layouts.map(e => e.tiles).join(", ")}`);
 		return;
 	}
 	solver.updateInput(inputElement.value);
+	updateURLParams();
 	solver.startSolving();
 });
 
-const showGradientElement = document.getElementById('show-gradient');
-const showLineElement = document.getElementById('show-line');
+const showGradientElement = document.getElementById("show-gradient");
+const showLineElement = document.getElementById("show-line");
 
 const optionChangeListener = () => {
 	solver.displayLastSolution();
-}
+};
 
-showGradientElement.addEventListener('change', optionChangeListener);
-showLineElement.addEventListener('change', optionChangeListener);
+showGradientElement.addEventListener("change", optionChangeListener);
+showLineElement.addEventListener("change", optionChangeListener);
--- js/solver.js
+++ js/solver.js
@@ -47,6 +47,60 @@ class Trie {
 	}
 }
 
+const UNDEFINED = 0;
+const TILE = 1;
+const VOID = -1;
+
+const layouts = [
+	{
+		width: 4,
+		height: 4,
+		tiles: 16,
+		tilemap: [
+			TILE, TILE, TILE, TILE,
+			TILE, TILE, TILE, TILE,
+			TILE, TILE, TILE, TILE,
+			TILE, TILE, TILE, TILE,
+		]
+	},
+	{
+		width: 5,
+		height: 5,
+		tiles: 20,
+		tilemap: [
+			VOID, TILE, TILE, TILE, VOID,
+			TILE, TILE, TILE, TILE, TILE,
+			TILE, TILE, VOID, TILE, TILE,
+			TILE, TILE, TILE, TILE, TILE,
+			VOID, TILE, TILE, TILE, VOID,
+		]
+	},
+	{
+		width: 5,
+		height: 5,
+		tiles: 21,
+		tilemap: [
+			TILE, TILE, VOID, TILE, TILE,
+			TILE, TILE, TILE, TILE, TILE,
+			VOID, TILE, TILE, TILE, VOID,
+			TILE, TILE, TILE, TILE, TILE,
+			TILE, TILE, VOID, TILE, TILE,
+		]
+	},
+	{
+		width: 5,
+		height: 5,
+		tiles: 25,
+		tilemap: [
+			TILE, TILE, TILE, TILE, TILE,
+			TILE, TILE, TILE, TILE, TILE,
+			TILE, TILE, TILE, TILE, TILE,
+			TILE, TILE, TILE, TILE, TILE,
+			TILE, TILE, TILE, TILE, TILE,
+		]
+	}
+];
+
 class Solver {
 	constructor(grid, overlay, results) {
 		this.gridElement = grid;
@@ -54,14 +108,16 @@ class Solver {
 		this.resultsElement = results;
 
 		this.#resizeOverlayElement();
-		this.overlayCtx = this.overlayElement.getContext('2d');
+		this.overlayCtx = this.overlayElement.getContext("2d");
+
+		this.layout = layouts[0];
 
 		this.lastShown = null;
 
 		this.dictionary = new Trie();
-		WORDLIST.forEach(word => this.dictionary.insert(word));
+		WORDLIST.forEach((word) => this.dictionary.insert(word));
 
-		this.input = '';
+		this.input = "";
 
 		this.results = [];
 		this.stopped = false;
@@ -69,36 +125,65 @@ class Solver {
 	}
 
 	#resetGridElement() {
-		for (const idx in this.gridElement.children) {
-			if (isNaN(idx)) {
+		const lastLayout = this.layout;
+		this.layout = layouts.find(e => e.tiles === this.input.length);
+		if (!this.layout) {
+			console.log("Error resetting grid element. Input is of unknown layout.");
+			return null;
+		}
+
+		if (lastLayout !== this.layout) {
+			this.gridElement.innerHTML = "";
+			let n = 0;
+			while (n++ < this.layout.height * this.layout.width) {
+				const gridChild = document.createElement("div");
+				gridChild.classList.add("grid-item");
+				gridChild.innerText = n;
+				if (this.layout.tilemap[n - 1] === VOID) gridChild.classList.add("grid-void");
+				this.gridElement.appendChild(gridChild);
+			}
+
+			this.gridElement.style.gridTemplateColumns = `repeat(${this.layout.width}, 1fr)`;
+			this.gridElement.style.gridTemplateRows = `repeat(${this.layout.height}, 1fr)`;
+		}
+
+		let idx = 0;
+		for (const n in this.gridElement.children) {
+			if (this.layout.tilemap[n] === TILE) {
+				this.gridElement.children[n].innerText = this.input[idx];
+				this.gridElement.children[n].style.backgroundColor = "#ccc";
+				idx++;
+			} else {
 				continue;
 			}
-			this.gridElement.children[idx].innerText = this.input[idx];
-			this.gridElement.children[idx].style.backgroundColor = '#ccc';
 		}
 	}
 
 	#drawSolutionOverlay(solution) {
-		this.overlayCtx.clearRect(0, 0, this.overlayElement.width, this.overlayElement.height);
+		this.overlayCtx.clearRect(
+			0,
+			0,
+			this.overlayElement.width,
+			this.overlayElement.height
+		);
 
-		const showLineElement = document.getElementById('show-line');
+		const showLineElement = document.getElementById("show-line");
 		if (showLineElement && !showLineElement.checked) {
 			return;
 		}
 
 		this.#resizeOverlayElement();
 
-
 		this.overlayCtx.beginPath();
 		this.overlayCtx.lineWidth = 4;
-		this.overlayCtx.strokeStyle = 'red';
+		this.overlayCtx.strokeStyle = "red";
 
 		const cellWidth = this.gridElement.children[0].offsetWidth;
 		const cellHeight = this.gridElement.children[0].offsetHeight;
 
 		const getCellCenter = (cellIdx) => {
-			const row = Math.floor(cellIdx / 4);
-			const col = cellIdx % 4;
+			const row = Math.floor(cellIdx / this.layout.width);
+			const col = cellIdx % this.layout.height;
 			const x = col * cellWidth + cellWidth / 2;
 			const y = row * cellHeight + cellHeight / 2;
 			return { x, y };
@@ -120,21 +205,31 @@ class Solver {
 
 			this.overlayCtx.beginPath();
 			this.overlayCtx.moveTo(toX, toY);
-			this.overlayCtx.lineTo(toX - headLength * Math.cos(angle - Math.PI / 6), toY - headLength * Math.sin(angle - Math.PI / 6));
+			this.overlayCtx.lineTo(
+				toX - headLength * Math.cos(angle - Math.PI / 6),
+				toY - headLength * Math.sin(angle - Math.PI / 6)
+			);
 			this.overlayCtx.moveTo(toX, toY);
-			this.overlayCtx.lineTo(toX - headLength * Math.cos(angle + Math.PI / 6), toY - headLength * Math.sin(angle + Math.PI / 6));
+			this.overlayCtx.lineTo(
+				toX - headLength * Math.cos(angle + Math.PI / 6),
+				toY - headLength * Math.sin(angle + Math.PI / 6)
+			);
 
 			this.overlayCtx.stroke();
 		};
 
 		const lastCellCenter = getCellCenter(solution[solution.length - 1]);
 		const prevLastCellCenter = getCellCenter(solution[solution.length - 2]);
-		drawArrow(prevLastCellCenter.x, prevLastCellCenter.y, lastCellCenter.x, lastCellCenter.y);
+		drawArrow(
+			prevLastCellCenter.x,
+			prevLastCellCenter.y,
+			lastCellCenter.x,
+			lastCellCenter.y
+		);
 	}
 
-
 	#highlightGridElements(solution) {
-		const showGradientElement = document.getElementById('show-gradient');
+		const showGradientElement = document.getElementById("show-gradient");
 
 		const getColor = (idx, total) => {
 			const hue = 120;
@@ -168,19 +263,23 @@ class Solver {
 	#resizeOverlayElement() {
 		const gridRect = this.gridElement.getBoundingClientRect();
 
-		this.overlayElement.style.width = gridRect.width + 'px';
-		this.overlayElement.style.height = gridRect.height + 'px';
+		this.overlayElement.style.width = gridRect.width + "px";
+		this.overlayElement.style.height = gridRect.height + "px";
 
 		this.overlayElement.width = gridRect.width;
 		this.overlayElement.height = gridRect.height;
 
-		this.overlayElement.style.top = window.scrollY + gridRect.top + 'px';
-		this.overlayElement.style.left = window.scrollX + gridRect.left + 'px';
+		this.overlayElement.style.top = window.scrollY + gridRect.top + "px";
+		this.overlayElement.style.left = window.scrollX + gridRect.left + "px";
 	}
 
-
 	getSolutionWord(solution) {
-		const word = solution.map(idx => this.input[idx]).join('');
+		const shiftAccountingVoid = (idx) => {
+			let idxClone = idx;
+			for (let i = 0; i < idxClone; i++) if (this.layout.tilemap[i] === VOID) idx--;
+			return idx;
+		}
+		const word = solution.map((idx) => this.input[shiftAccountingVoid(idx)]).join("");
 		return word;
 	}
 
@@ -213,14 +312,14 @@ class Solver {
 
 	getMatrixFromInput() {
 		const matrix = [];
-		for (let i = 0; i < 4; i++) {
-			matrix.push(this.input.slice(i * 4, (i + 1) * 4));
+		for (let i = 0; i < this.layout.height; i++) {
+			matrix.push(this.input.slice(i * this.layout.width, (i + 1) * this.layout.width));
 		}
 		return matrix;
 	}
 
 	#resetResultsElement() {
-		this.resultsElement.innerHTML = '';
+		this.resultsElement.innerHTML = "";
 	}
 
 	#updateResultsElement() {
@@ -231,12 +330,12 @@ class Solver {
 		});
 
 		for (const result of sortedResults) {
-			const li = document.createElement('li');
+			const li = document.createElement("li");
 			li.innerText = this.getSolutionWord(result);
-			li.addEventListener('click', () => {
+			li.addEventListener("click", () => {
 				this.#resetGridElement();
 				this.#showSolution(result);
-				const currentWordElement = document.getElementById('current-word');
+				const currentWordElement = document.getElementById("current-word");
 				if (currentWordElement) {
 					currentWordElement.innerText = this.getSolutionWord(result);
 				}
@@ -246,24 +345,28 @@ class Solver {
 	}
 
 	#updateNodesElement() {
-		const nodesElement = document.getElementById('nodes');
+		const nodesElement = document.getElementById("nodes");
 		if (nodesElement) {
 			nodesElement.innerText = this.nodes;
 		}
 	}
 
 	#updateTimeElement(start) {
-		const timeElement = document.getElementById('time');
+		const timeElement = document.getElementById("time");
 		if (timeElement) {
-			timeElement.innerText = `${(performance.now() - start)}ms`;
+			timeElement.innerText = `${performance.now() - start}ms`;
 		}
 	}
 
 	#updateScoreElement() {
-		const scoreElement = document.getElementById('score');
+		const scoreElement = document.getElementById("score");
 		if (scoreElement) {
-			const scores = [0, 0, 0, 100, 400, 800, 1400, 1800, 2200, 2600, 3000, 3400];
-			scoreElement.innerText = this.results.map(result => scores[result.length]).reduce((a, b) => a + b, 0);
+			const scores = [
+				0, 0, 0, 100, 400, 800, 1400, 1800, 2200, 2600, 3000, 3400,
+			];
+			scoreElement.innerText = this.results
+				.map((result) => scores[result.length])
+				.reduce((a, b) => a + b, 0);
 		}
 	}
 
@@ -272,7 +375,12 @@ class Solver {
 		this.results = [];
 		this.nodes = 0;
 		this.#resetGridElement();
-		this.overlayCtx.clearRect(0, 0, this.overlayElement.width, this.overlayElement.height);
+		this.overlayCtx.clearRect(
+			0,
+			0,
+			this.overlayElement.width,
+			this.overlayElement.height
+		);
 		this.#resetResultsElement();
 		this.#solve();
 	}
@@ -285,9 +393,14 @@ class Solver {
 		const start = performance.now();
 
 		const directions = [
-			[-1, -1], [-1, 0], [-1, 1],
-			[0, -1], [0, 1],
-			[1, -1], [1, 0], [1, 1]
+			[-1, -1],
+			[-1, 0],
+			[-1, 1],
+			[0, -1],
+			[0, 1],
+			[1, -1],
+			[1, 0],
+			[1, 1],
 		];
 
 		const visited = new Set();
@@ -295,14 +408,20 @@ class Solver {
 		const dfs = (i, j, path) => {
 			if (this.stopped) return;
 
+			if (this.layout.tilemap[i * this.layout.width + j] === VOID) return;
+
 			const key = `${i},${j}`;
 			if (visited.has(key)) return;
 
-			path.push(i * 4 + j);
+			path.push(i * this.layout.width + j);
 			visited.add(key);
 
 			const currentWord = this.getSolutionWord(path);
-			if (currentWord.length >= 3 && this.dictionary.search(currentWord) && !this.results.some(result => result.toString() === path.toString())) {
+			if (
+				currentWord.length >= 3 &&
+				this.dictionary.search(currentWord) &&
+				!this.results.some((result) => result.toString() === path.toString())
+			) {
 				this.results.push([...path]);
 			}
 
@@ -311,7 +430,7 @@ class Solver {
 					const ni = i + di;
 					const nj = j + dj;
 
-					if (ni >= 0 && ni < 4 && nj >= 0 && nj < 4) {
+					if (ni >= 0 && ni < this.layout.width && nj >= 0 && nj < this.layout.height) {
 						dfs(ni, nj, path);
 						this.nodes++;
 					}
@@ -322,8 +441,8 @@ class Solver {
 			path.pop();
 		};
 
-		for (let i = 0; i < 4; i++) {
-			for (let j = 0; j < 4; j++) {
+		for (let i = 0; i < this.layout.width; i++) {
+			for (let j = 0; j < this.layout.height; j++) {
 				dfs(i, j, []);
 			}
 		}
